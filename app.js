46497bbd42c8a88702bf475a2dd5b48eb695903e
sharvarichinchkar13-design <sharvarichinchkar13@gmail.com>
2026-04-13 13:07:04 +0530

    added app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello from Docker container ");
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.end("Hello Docker App!");
+}).listen(3000);
+
+console.log("Server running on port 3000");
